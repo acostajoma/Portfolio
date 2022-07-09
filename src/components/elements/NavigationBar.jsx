7146bbecf035c2324c822b3540e5aaaaafffd9ac
Jose Acosta <acostajoma9@gmail.com>
2022-07-09 13:02:44 -0600

    Added effect to navigation bar urls

--- src/components/elements/NavigationBar.jsx
+++ src/components/elements/NavigationBar.jsx
@@ -1,12 +1,10 @@
 const NavigationBar = () => {
   return (
-    <nav>
-        <ul className="flex text-2xl text-white">
-            <li className="py-9 px-5"><a href="#about">About me</a></li>
-            <li className="py-9 px-5"><a href="#projects">Projects</a></li>
-            <li className="py-9 px-5"><a href="#certifications">Certifications</a></li>
-            <li className="py-9 px-5"><a href="#contact">Contact me</a></li>
-        </ul>
+    <nav className="flex py-5 text-2xl text-white">
+      <a className="py-2 mx-7 relative after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-full after:h-1 after:bg-white after:scale-x-0 after:origin-right after:transition-transform ease-in after:duration-500 hover:after:scale-x-100 hover:after:origin-left" href="#about">About me</a>
+      <a className="py-2 mx-7 relative after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-full after:h-1 after:bg-white after:scale-x-0 after:origin-right after:transition-transform ease-in after:duration-500 hover:after:scale-x-100 hover:after:origin-left" href="#projects">Projects</a>
+      <a className="py-2 mx-7 relative after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-full after:h-1 after:bg-white after:scale-x-0 after:origin-right after:transition-transform ease-in after:duration-500 hover:after:scale-x-100 hover:after:origin-left" href="#certifications">Certifications</a>
+      <a className="py-2 mx-7 relative after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-full after:h-1 after:bg-white after:scale-x-0 after:origin-right after:transition-transform ease-in after:duration-500 hover:after:scale-x-100 hover:after:origin-left" href="#contact">Contact me</a>
     </nav>
   )
 }
--- src/components/elements/TypingEffect.jsx
+++ src/components/elements/TypingEffect.jsx
@@ -3,32 +3,22 @@ import { useEffect, useState } from "react"
 const TypingEffect = ({textToType}) => {
 
     const [text, setText] = useState('')
-    const [showPipe, setShowPipe] = useState(true)
 
     useEffect( () => {
         typeWriter()
     }, [])
 
-    // useEffect(() => {
-
-    // }, [text])
     
     const typeWriter = (i=0, typed='') => {
         if ( i < textToType.length) {
             typed = `${typed}${textToType.charAt(i)}`
             setText(typed)
-            i++
-            setTimeout(() => typeWriter(i, typed),120);
-        } else {
-            blinkingPipe()
-        }
+            setTimeout(() => typeWriter(++i, typed),120);
+        } 
     }
 
-    const blinkingPipe = () => {}
-
-
     return (
-        <h2 className="text-white text-lg">{text}<span className={ showPipe ? '' : 'hidden'}>|</span></h2>
+        <h2 className="text-white text-lg">{text}<span className="animate-ping">_</span></h2>
     )
 }
 
