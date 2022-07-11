ccbb0a68a594f150dd3355dc053c74f8d0bd07e4
Jose Acosta <acostajoma9@gmail.com>
2022-07-11 15:52:22 -0600

    Added Social section and an effect using the oppacity, fixed how the navigation bar should look on smaller screens

--- src/components/elements/NavigationBar.jsx
+++ src/components/elements/NavigationBar.jsx
@@ -6,28 +6,27 @@ const NavigationBar = () => {
   const [showNavBar, setShowNavBar] = useState(false)
 
   return (
-    <nav className="py-3 fixed z-10 top-0 w-full overflow-hidden bg-gray-800 text-2xl text-white">
+    <div className='py-3 fixed z-10 top-0 w-full overflow-hidden bg-gray-600 text-xl lg:text-2xl text-white'>
 
-      <div className="container m-auto">
+      <div className={` ${!showNavBar ? 'h-14' : 'h-64'} lg:h-full transition-[height] ease-in-out duration-200  container m-auto`}>
+
         <button 
-          className="my-2 mx-5 w-fit p-2 lg:hidden rounded-full hover:bg-gray-600" 
-          onClick={() => setShowNavBar(!showNavBar) }
-        >
+          className='my-2 mx-5 w-fit p-2 lg:hidden rounded-full hover:bg-gray-600'
+          onClick={() => setShowNavBar(!showNavBar) }>
           <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
             <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
           </svg>
-
         </button>
 
-        <div className={`${showNavBar ? 'flex' : 'hidden'} transition-[height] ease-in duration-500  flex-col lg:flex lg:flex-row`}>
+        <nav className='flex flex-col lg:flex lg:flex-row'>
           <PrettyLink text="About Me" linkref="#about"/>
           <PrettyLink text="Projects" linkref="#projects"/>
           <PrettyLink text="Certifications" linkref="#certifications"/>
           <PrettyLink text="Contact me" linkref="#contact"/>
-        </div>
+        </nav>
 
       </div>
-    </nav>
+    </div>
   )
 }
 
--- src/components/elements/TypingEffect.jsx
+++ src/components/elements/TypingEffect.jsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react"
+import Socials from "../elements/Socials"
 
 const TypingEffect = ({textToType}) => {
 
     const [text, setText] = useState('')
+    const [socialsVisible, setSocialsVisible ] = useState(false)
 
     useEffect( () => {
         typeWriter()
@@ -14,13 +16,19 @@ const TypingEffect = ({textToType}) => {
         if ( i < textToType.length) {
             typed = `${typed}${textToType.charAt(i)}`
             setText(typed)
-            setTimeout(() => typeWriter(++i, typed),120);
+            return setTimeout(() => typeWriter(++i, typed),110);
         } 
+        setSocialsVisible(true)
     }
 
     return (
-        <div className="h-full font-MonoJet container m-auto">
-            <h1 className="text-white text-3xl whitespace-pre-line mx-7 relative top-[40%]">{text}<span className="animate-ping">_</span></h1>
+        <div className="h-full font-MonoJet container m-auto relative top-[45%]">
+            <div className="text-white text-3xl sm:text-4xl whitespace-pre-line mx-7 flex flex-col lg:flex-row lg:justify-between">
+                <h1>{text}<span className="animate-ping">_</span></h1>
+                <Socials 
+                    socialsVisible={socialsVisible}
+                />
+            </div>
         </div>
     )
 }
